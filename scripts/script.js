ee588e5b947b0df5a2585b0ac876edb0ead97695
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-08-31 14:00:25 +0700

    feat: Add script.js

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+const myLibrary = [];
+
+// Book constructor
+function Book(title, author, genre) {
+  this.title = title;
+  this.author = author;
+  this.genre = genre;
+}
+
+const addBookToLibrary = (book) => {
+  myLibrary.push(book);
+};
+
+const addBooksToList = (...books) => {
+  const uList = document.querySelector('.book-list');
+
+  for (let i = 0; i < books.length; i += 1) {
+    const li = document.createElement('li');
+
+    li.textContent = JSON.stringify(books[i]);
+    uList.appendChild(li);
+  }
+};
+
+const arr = [1, 3, 8, 5, 6, 7, 2];
+
+const book1 = new Book('English For Children', 'Unknown', 'Educational');
+addBookToLibrary(book1);
+const book2 = {
+  title:'Programming',
+  author: 'Unknown',
+  genre: 'Educational'
+};
+const book3 = {
+  title:'Programming',
+  author: 'Unknown',
+  genre: 'Educational'
+};
+addBookToLibrary(book2);
+
+addBooksToList(book1, book2);
+addBooksToList(book3);
