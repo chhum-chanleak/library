97e01c55ce17ec45ae2526b402766cdf9401e9e0
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-09-02 15:34:17 +0700

    fix: Update handleRemoveButton, so it removes bother object on display and in myLibrary

--- scripts/script.js
+++ scripts/script.js
@@ -27,16 +27,19 @@ const addBooksToLibrary = (...book) => {
 };
 
 // Display all books in the library to list
-const addBooksToList = (library) => {
+const addBooksToList = (...book) => {
   const uList = document.querySelector('.book-list');
 
-  for (let i = 0; i < library.length; i += 1) {
+  for (let i = 0; i < book.length; i += 1) {
     const li = document.createElement('li');
     const span = document.createElement('span');
 
-    span.textContent = JSON.stringify(library[i]);
+    span.textContent = JSON.stringify(book[i]);
+    li.setAttribute('id', `${book[i].id}`);
     li.appendChild(span);
     uList.appendChild(li);
+    applyRemoveButtonToNode(span);
+    applyReadButtonToNode(span);
   }
 };
 
@@ -52,7 +55,7 @@ const toggleReadStatus = (obj) => {
   }  
 };
 
-const handleNewBook = () => {
+const handlePublishNewBookButton = () => {
   const form = document.querySelector('form');
   const publishNewBookButton = document.querySelector('#new-book');
 
@@ -65,29 +68,12 @@ const handleNewBook = () => {
   }
 };
 
-const handleRemoveButton = () => {
-  const removeButtons = document.querySelectorAll('.btn.remove');
-  const readButtons = document.querySelectorAll('.btn.read');
-  const lists = document.querySelectorAll('.library li');
-  
-  // Apply event listener
-  for (let i = 0; i < removeButtons.length; i += 1) {
-    removeButtons[i].addEventListener('click', () => {
-      lists[i].remove();
-      readButtons[i].remove();
-      removeButtons[i].remove();
-      console.log(`${readButtons[i].id} has been removed.`);
-      removeItem(readButtons[i].id, myLibrary);
-    })   
-  }
-};
-
 // Remove item from Library
-const removeItem = (id, ...object) => {
-  for (let i = 0; i < object.length; i += 1) {
-    for (const prop in object[i]) {
-      if (id === object[i][prop].id) {
-        object[i].splice(+prop[i], 1);        
+const removeItem = (id, ...arrayObj) => {
+  for (let i = 0; i < arrayObj.length; i += 1) {
+    for (const prop in arrayObj[i]) {
+      if (id === arrayObj[i][prop].id) {
+        arrayObj[i].splice(+prop[i], 1);        
       }
     }
   }
@@ -106,12 +92,24 @@ const createBookFromForm = (event) => {
   }
   
   book = new Book(...formData);
+  addBooksToList(book);
   addBooksToLibrary(book);
-  addBooksToList(myLibrary);
+  handleRemoveButton();
 };
 
+const handleRemoveButton = () => {
+  const removeButtons = document.querySelectorAll('.btn.remove');
 
-const handleSubmitButton = () => {
+  for (let i = 0; i < removeButtons.length; i += 1) {
+    removeButtons[i].addEventListener('click', () => {
+      removeItem(removeButtons[i].parentNode.getAttribute('id'), myLibrary);
+      removeButtons[i].parentNode.remove();
+    });
+  }
+
+};
+
+const handleSubmitButton = (event) => {
   const submitButton = document.querySelector('.submit');
   const inputs = document.querySelectorAll('form input');
 
@@ -123,7 +121,7 @@ const handleSubmitButton = () => {
   })
 };
 
-const handleRead = () => {
+const handleReadButton = () => {
   const readButtons = document.querySelectorAll('.book-list .btn.read');
   const spans = document.querySelectorAll('.book-list span');
 
@@ -154,45 +152,29 @@ const handleRead = () => {
 };
 
 // Attach each book with a remove button
-const applyRemoveButton = () => {
-  const lists = document.querySelectorAll('li');
-  
-  for (let i = 0; i < lists.length; i += 1) {
-    const button = document.createElement('button');
+const applyRemoveButtonToNode = (node) => {
+  const button = document.createElement('button');
 
-    button.textContent = 'Remove';
-    button.setAttribute('id', myLibrary[i].id);
-    button.setAttribute('class', 'btn remove');
-    lists[i].insertAdjacentElement('afterend', button); 
-  }
+  button.textContent = 'Remove';
+  button.setAttribute('class', 'btn remove');
+  node.insertAdjacentElement('afterend', button);
+  handleRemoveButton();
 };
 
 // Attach each book with a remove button
-const applyReadButton = () => {
-  const lists = document.querySelectorAll('li');
+const applyReadButtonToNode = (node) => {
+  const button = document.createElement('button');
 
-  for (let i = 0; i < lists.length; i += 1) {
-    const button = document.createElement('button');
-
-    button.textContent = 'Read';
-    button.setAttribute('id', myLibrary[i].id);
-    button.setAttribute('class', 'btn read');
-    lists[i].insertAdjacentElement('afterend', button);  
-  }
+  button.textContent = 'Read';
+  button.setAttribute('class', 'btn read');
+  node.insertAdjacentElement('afterend', button);
 };
 
-addBooksToList(myLibrary);
-
-// Attach each book with Remove and Read buttons
-applyRemoveButton();
-applyReadButton();
 // Handle buttons
-handleRead();
 handleSubmitButton();
-handleRemoveButton();
 // Apply event listener to NEW BOOK button
 const newBookBtn = document.querySelector('#new-book');
-newBookBtn.addEventListener('click', handleNewBook);
+newBookBtn.addEventListener('click', handlePublishNewBookButton);
 // Apply event listener to Submit button
 const submitButton = document.querySelector('.submit');
 submitButton.addEventListener('click', handleSubmitButton);
